18b867e60fa9d8eded971f6f499dfde8c33a74cc
Alex <alexvatti@gmail.com>
2024-05-20 14:55:20 +0530

    Movie Review Classify using Flask

---
+++ 01_Movie-Review-App.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pickle
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+
+from flask import Flask, request, render_template
+
+app=Flask(__name__)
+ml_model = pickle.load(open('movie_review_rbf_model.pkl', 'rb'))
+vectorizer_model = pickle.load(open('count_vectorizer_movie_review.pkl','rb'))
+
+
+def pre_processing(text):
+    words =  word_tokenize(text) 
+    eng_stopwords=stopwords.words("english")
+    tokens = [word for word in words if word.isalnum()]
+    lower_tokens = [word.lower() for word in tokens ]
+
+    no_punctuations_stopwords_tokens = [token for  token in lower_tokens if token not in eng_stopwords]
+    return " ".join(no_punctuations_stopwords_tokens)
+
+@app.route('/')      # decorator
+def home():
+      review_image = 'static/sentiment.jpeg'
+      return render_template('index.html',review_image=review_image)
+    
+    
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+
+    text = request.form['Movie Review']
+        #text=list(request.form.values())[0]
+    #print(text)
+    processed_text = pre_processing(text)
+    X = vectorizer_model.transform([processed_text])
+    prediction = ml_model.predict(X)
+
+    ans =prediction[0]
+    #print(ans)
+    if ans == 1: 
+        review='Positive' 
+    else:
+        review='Negative'
+    review_image = 'static/sentiment.jpeg'
+    color = 'lightgreen' if prediction[0] == 'positive' else 'red'
+    return render_template('index.html', review_image=review_image,prediction_text='Moview Review Classified As {} '.format(review))
+
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=5000)
---
+++ ReadMe.txt
@@ -0,0 +1,15 @@
+
+Step 1: Run the following command in the command prompt:
+pip install -r requirements.txt
+
+Step 2: Open the movie-review-using-flask app with VS Code.
+
+Step 3:
+
+1.Open the Anaconda command prompt.
+2.Navigate to the directory of the movie-review-using-flask app:
+cd <path  movie-review-using-flask>
+3. Run the flask app: python python .\01_Movie-Review-App.py
+
+Step 4: copy the IP address with port number , copy paste in web browser
+ Enter the input Text -Movie Review  and then click Predict button.
